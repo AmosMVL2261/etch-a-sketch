bbe84a256a6b14f8b76b1db9fee178dd3cc8f18d
Amós Vega <refi2261@gmail.com>
2022-05-10 16:24:04 -0500

    rgba logic added

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
+const mode = "multicolor";
 const container = document.getElementById("gridDiv");
 
+//we add class gridbox just for css, while, id is for individual color change
 function createGrid(columns, rows){
     container.style.setProperty('--rows', rows);
     container.style.setProperty('--columns', columns);
@@ -12,12 +14,44 @@ function createGrid(columns, rows){
 
 createGrid(10,10);
 
+//Create addEventListener for each div created
 var items = document.getElementsByClassName('gridBox');
 for (var i = 0; i < items.length; i++) {
-  items[i].addEventListener('mouseover', paintBlack);
+  items[i].addEventListener('mouseover', selection);
 }
 
-function paintBlack() {
-    const box =document.getElementById(this.id);
+//Default option
+function paintBlack(identifier) {
+    const box=document.getElementById(identifier);
     box.style.backgroundColor="black";
+}
+
+//Random number from 0 to 255 (inclusive), for rgba codes
+function getRandomNumber(){
+    let min = 0;
+    let max = 255;
+    return (Math.random()*(max-min+1)+min);
+}
+
+//Random rgb background color
+function multiColor(identifier) {
+    const box=document.getElementById(identifier);
+    let red = getRandomNumber();
+    let green = getRandomNumber();
+    let blue = getRandomNumber();
+    box.style.backgroundColor='rgb(' + red + ',' + green + ',' + blue + ')';
+}
+
+//User selection
+function selection(){
+    switch(mode){
+        case "black":
+            paintBlack(this.id);
+            break;
+        case "multicolor":
+            multiColor(this.id);
+            break;
+        default:
+            paintBlack(this.id);
+    }
 }
