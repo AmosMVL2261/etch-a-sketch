ddc21623937a0e9f2bfaa8da3cf5424cbd1c5111
Amós Vega <refi2261@gmail.com>
2022-05-11 14:57:18 -0500

    All sections done, except a nice interface

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,9 @@
 let mode = "black";
 let temporalMode = "black";
-let totalRows = 50;
-let totalColumns = 50;
+let totalRows = 30;
+let totalColumns = 30;
 const container = document.getElementById("gridDiv");
+const dimension = document.getElementsByClassName("actualSize");
 
 //Added class gridbox to divs just for css, while, id is for individual color change
 function createGrid(rows, columns){
@@ -13,15 +14,21 @@ function createGrid(rows, columns){
       container.appendChild(cell).className = "gridBox";
       container.appendChild(cell).setAttribute("id",i.toString());
     };
+    //Actual dimension section
+    for (let i = 0; i < dimension.length; i++){
+        dimension[i].innerText = totalColumns.toString();
+    }
+    dimension.i
+    //Create addEventListener for each div created
+    let items = document.getElementsByClassName('gridBox');
+    for (let i = 0; i < items.length; i++) {
+        items[i].addEventListener('mouseover', selection);
+    }
 }
 
+//Initial grid
 createGrid(totalRows,totalColumns);
 
-//Create addEventListener for each div created
-let items = document.getElementsByClassName('gridBox');
-for (var i = 0; i < items.length; i++) {
-  items[i].addEventListener('mouseover', selection);
-}
 
 //Default option
 function paintBlack(identifier) {
@@ -91,9 +98,28 @@ function bReset(){
     selection();
 }
 
+function changeGridSize(){
+    let newSize = Number(window.prompt("New grid size:"));
+    //Grid limited from 1 to 100
+    if(newSize>0 && newSize<=100){
+        totalRows = newSize;
+        totalColumns = newSize;
+        //Remove all old divs to create new later
+        while (container.firstChild) {
+            container.removeChild(container.lastChild);
+        }
+        createGrid(totalRows,totalColumns);
+    }else{
+        //If the user put an invalid number or a word, it doesn't create a new grid
+        window.alert("Invalid input");
+    }
+}
+
 const buttonBlack = document.getElementById("buttonBlack");
 const buttonColor = document.getElementById("buttonColor");
 const buttonReset = document.getElementById("buttonReset");
+const gridSize = document.getElementById("newSize");
 buttonBlack.addEventListener("click",bBlack);
 buttonColor.addEventListener("click",bColor);
 buttonReset.addEventListener("click",bReset);
+gridSize.addEventListener("click",changeGridSize);
