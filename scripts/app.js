9a73ef3ffca2c31957c1636cb6c54221b9094d17
Amós Vega <refi2261@gmail.com>
2022-05-10 20:00:48 -0500

    buttons added

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,11 @@
-let mode = "multicolor";
+let mode = "black";
+let temporalMode = "black";
+let totalRows = 50;
+let totalColumns = 50;
 const container = document.getElementById("gridDiv");
 
 //Added class gridbox to divs just for css, while, id is for individual color change
-function createGrid(columns, rows){
+function createGrid(rows, columns){
     container.style.setProperty('--rows', rows);
     container.style.setProperty('--columns', columns);
     for (let i = 0; i < (rows * columns); i++) {
@@ -12,7 +15,7 @@ function createGrid(columns, rows){
     };
 }
 
-createGrid(10,10);
+createGrid(totalRows,totalColumns);
 
 //Create addEventListener for each div created
 let items = document.getElementsByClassName('gridBox');
@@ -22,8 +25,11 @@ for (var i = 0; i < items.length; i++) {
 
 //Default option
 function paintBlack(identifier) {
-    const box=document.getElementById(identifier);
-    box.style.backgroundColor="black";
+    //If you clicked a button, you're not pointing nothing, so gets error. This prevent that error
+    if(identifier!==undefined){
+        const box=document.getElementById(identifier);
+        box.style.backgroundColor="black";
+    }
 }
 
 //Random number from 0 to 255 (inclusive), for rgba codes
@@ -35,11 +41,13 @@ function getRandomNumber(){
 
 //Random rgb background color
 function multiColor(identifier) {
-    const box=document.getElementById(identifier);
-    let red = getRandomNumber();
-    let green = getRandomNumber();
-    let blue = getRandomNumber();
-    box.style.backgroundColor='rgb(' + red + ',' + green + ',' + blue + ')';
+    if(identifier!==undefined){
+        const box=document.getElementById(identifier);
+        let red = getRandomNumber();
+        let green = getRandomNumber();
+        let blue = getRandomNumber();
+        box.style.backgroundColor='rgb(' + red + ',' + green + ',' + blue + ')';
+    }
 }
 
 
@@ -47,9 +55,9 @@ function paintWhite(){
     let items = document.getElementsByClassName('gridBox');
     for (var i = 0; i < items.length; i++) {
       items[i].style.backgroundColor="white";
-    } 
-    console.log("done");
-    mode = "black";
+    }
+    console.log();
+    mode = temporalMode; //Restore the last mode before erase
 }
 
 //User selection
@@ -67,3 +75,25 @@ function selection(){
             paintBlack(this.id);
     }
 }
+
+
+function bBlack(){
+    mode = "black";
+    selection();
+}
+function bColor(){
+    mode = "multicolor";
+    selection();
+}
+function bReset(){
+    temporalMode = mode; //conserve the actual mode before erase all
+    mode = "erase";
+    selection();
+}
+
+const buttonBlack = document.getElementById("buttonBlack");
+const buttonColor = document.getElementById("buttonColor");
+const buttonReset = document.getElementById("buttonReset");
+buttonBlack.addEventListener("click",bBlack);
+buttonColor.addEventListener("click",bColor);
+buttonReset.addEventListener("click",bReset);
